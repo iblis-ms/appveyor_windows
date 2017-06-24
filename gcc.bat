e5bc0c3481381e53975c494252db01262fae6ab5
iblis <marcin.serwach@gmail.com>
2017-06-24 14:01:15 +0200

    remove not required commands

--- gcc.bat
+++ gcc.bat
@@ -2,6 +2,9 @@
 SET "CC=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin\gcc.exe"
 SET "CXX=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin\g++.exe"
 
+ECHO CC=%CC%
+ECHO CXX=%CXX%
+
 MKDIR "output_gcc"
 CD "output_gcc"
 cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release ..\code 
