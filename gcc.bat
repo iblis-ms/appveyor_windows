86612158ac12d430d9e67c3c3aaa49020f6da284
iblis <marcin.serwach>
2021-09-05 16:11:34 +0200

    rerun

--- gcc.bat
+++ gcc.bat
@@ -1,4 +1,5 @@
 
+dir "C:\mingw-w64"
 SET "CC=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin\gcc.exe"
 SET "CXX=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin\g++.exe"
 SET "PATH=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin;C:\Program Files (x86)\CMake\bin"
