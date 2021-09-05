3ca85047b2efbbf6dee7f8a8bf68a1627b4f9271
iblis <marcin.serwach>
2021-09-05 17:39:55 +0200

    llog1

--- gcc.bat
+++ gcc.bat
@@ -1,4 +1,5 @@
 
+ECHO %PATH%
 dir "C:\mingw-w64"
 SET "CC=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin\gcc.exe"
 SET "CXX=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin\g++.exe"
